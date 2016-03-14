0221bbd7bffa88a041370a71f1815fa6e82de8a4
idan573 <hagit058@gmail.com>
2016-03-15 01:44:26 +0200

    Final Commit QTT#001

--- HumanVoiceToComputerVoice/MainWindow.xaml.cs
+++ HumanVoiceToComputerVoice/MainWindow.xaml.cs
@@ -1,21 +1,65 @@
 ﻿using System;
-using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Speech.Synthesis;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using System.Windows.Shapes;
 using Microsoft.ProjectOxford.SpeechRecognition;
 
 namespace HumanVoiceToComputerVoice
 {
     public partial class MainWindow : Window
     {
-        private string _recoLanguage = "en-us";
-        private readonly string _subscriptionKey = ConfigurationManager.AppSettings["primaryKey"];
-        private string _recognizedToken;
-        private MicrophoneRecognitionClient _micClient;
-        
+        private SpeechRecognitionHandler _speechRecognition;
+        public ErrorsHandler ErrorHandler { get; set; }
+
+        #region Error Handler
+
+        public class ErrorsHandler
+        {
+            public ErrorsHandler()
+            {
+                Is64 = Environment.Is64BitProcess;
+                IsInternetValid = CheckInternetConnection();
+            }
+            private bool CheckInternetConnection()
+            {
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        using (var stream = client.OpenRead("http://www.google.com"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            public bool IsMicConnected { get; set; } = true;
+            public bool Is64 { get; }
+            public bool IsInternetValid { get; }
+            public bool IsPrimaryKeyIsValid { get; set; } = true;
+
+            public bool CanConnect
+            {
+                get { return IsMicConnected && Is64 && IsInternetValid && IsPrimaryKeyIsValid; }
+            }
+        }
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,159 +70,73 @@ namespace HumanVoiceToComputerVoice
             BtMicrophone.IsEnabled = false;
             BtComputerSpeak.IsEnabled = false;
 
-            if (_micClient == null)
-            {
-                if (TextBoxLongDict.IsChecked != null && !TextBoxLongDict.IsChecked.Value)
-                {
-                    _micClient = CreateMicrophoneRecoClient(SpeechRecognitionMode.ShortPhrase, _recoLanguage, _subscriptionKey);
-                }
-                else
-                {
-                    _micClient = CreateMicrophoneRecoClient(SpeechRecognitionMode.LongDictation, _recoLanguage, _subscriptionKey);
-                }
-            }
-
-            _micClient.StartMicAndRecognition();
-
+            _speechRecognition.StartSpeechAndRecognition();
         }
-
-        MicrophoneRecognitionClient CreateMicrophoneRecoClient(SpeechRecognitionMode recoMode, string language, string subscriptionKey)
-        {
-            MicrophoneRecognitionClient micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
-                recoMode,
-                language,
-                subscriptionKey);
-
-            // Event handlers for speech recognition results
-            micClient.OnMicrophoneStatus += OnMicrophoneStatus;
-            micClient.OnPartialResponseReceived += OnPartialResponseReceivedHandler;
-
-            if (recoMode == SpeechRecognitionMode.ShortPhrase)
-            {
-                micClient.OnResponseReceived += OnMicShortPhraseResponseReceivedHandler;
-            }
-            else
-            {
-                micClient.OnResponseReceived += OnMicLongPhraseResponseReceivedHandler;
-            }
-
-            return micClient;
-        }
-
-        void OnMicShortPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
-        {
-            var callback = (Action)(() =>
-            {
-                Console.WriteLine("--- OnMicShortPhraseResponseReceivedHandler ---");
-
-                _micClient.EndMicAndRecognition();
-
-                _micClient.Dispose();
-                _micClient = null;
-
-                WriteResponseResult(e);
-
-                UpdateUi();
-            });
-
-            Dispatcher.Invoke(callback);
-        }
-
-        private void UpdateUi()
-        {
-            TextRecordedToken.Text = _recognizedToken;
-
-            if (!string.IsNullOrEmpty(_recognizedToken))
-            {
-                BtComputerSpeak.IsEnabled = true;
-            }
-
-            BtMicrophone.IsEnabled = true;
-        }
-
-        void OnMicLongPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
-        {
-            Console.WriteLine("--- OnMicLongPhraseResponseReceivedHandler ---");
-            if (e.PhraseResponse.RecognitionStatus == RecognitionStatus.EndOfDictation ||
-                e.PhraseResponse.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout)
-            {
-                Dispatcher.Invoke((Action)(() =>
-                {
-                    UpdateUi();
-
-                    // we got the final result, so it we can end the mic reco.  No need to do this
-                    // for dataReco, since we already called endAudio() on it as soon as we were done
-                    // sending all the data.
-                }));
-            }
-            WriteResponseResult(e);
-        }
-
-        void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
-        {
-            Console.WriteLine("--- Microphone status change received by OnMicrophoneStatus() ---");
-            Console.WriteLine("********* Microphone status: {0} *********", e.Recording);
-            if (e.Recording)
-            {
-                Console.WriteLine("Please start speaking.");
-            }
-            Console.WriteLine();
-        }
-
-        private void WriteResponseResult(SpeechResponseEventArgs e)
-        {
-            var recognizedPhrases = e.PhraseResponse.Results;
-            var length = recognizedPhrases.Length;
-            if (length == 0)
-            {
-                Console.WriteLine("No phrase resonse is available.");
-            }
-            else
-            {
-                Console.WriteLine("********* Final n-BEST Results *********");
-                for (int i = 0; i < length; i++)
-                {
-                    Console.WriteLine("[{0}] Confidence={1}, Text=\"{2}\"",
-                                    i, recognizedPhrases[i].Confidence,
-                                    recognizedPhrases[i].DisplayText);
-                }
-
-                _recognizedToken = recognizedPhrases[0].DisplayText; 
-
-                Console.WriteLine();
-            }
-        }
-
-        void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
-        {
-            Console.WriteLine("--- Partial result received by OnPartialResponseReceivedHandler() ---");
-            Console.WriteLine("{0}", e.PartialResult);
-            Console.WriteLine();
-        }
-
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 DragMove();
         }
-
         private void OnCancel(object sender, RoutedEventArgs e)
         {
             Close();
         }
-
         private void OnPlayButton(object sender, RoutedEventArgs e)
         {
-            SpeechSynthesizer  reader = new SpeechSynthesizer();
+            SpeechSynthesizer reader = new SpeechSynthesizer();
 
-            if(!string.IsNullOrEmpty(_recognizedToken))
-                reader.SpeakAsync(_recognizedToken);
+            if (!string.IsNullOrEmpty(_speechRecognition.RecognizedToken))
+                reader.SpeakAsync(_speechRecognition.RecognizedToken);
         }
-
         private void OnLoad(object sender, RoutedEventArgs e)
         {
-            BtComputerSpeak.IsEnabled = false;
-        }
+            ErrorHandler = new ErrorsHandler();
 
+            if (ErrorHandler.Is64 && ErrorHandler.IsInternetValid)
+                _speechRecognition = new SpeechRecognitionHandler(this);
+
+            BtComputerSpeak.IsEnabled = false;
+
+            if (!ErrorHandler.CanConnect)
+            {
+                TxtConnectedStatus.Content = "Not Connected";
+
+                TxtConnectedStatus.Foreground = new SolidColorBrush(Colors.Red);
+
+                EllipseStatusIndicator.Fill = new SolidColorBrush(Colors.Red);
+
+                return;
+            }
+
+            TxtConnectedStatus.Content = "Connected";
+            TxtConnectedStatus.Foreground = new SolidColorBrush(Colors.Green);
+            EllipseStatusIndicator.Fill = new SolidColorBrush(Colors.Green);
+        }
+        private void OnInfo(object sender, RoutedEventArgs e)
+        {
+            string message;
+            if (!ErrorHandler.Is64)
+            {
+                message = "Please use x64 device";
+            }
+            else if (!ErrorHandler.IsInternetValid)
+            {
+                message = "No internet connection";
+            }
+            else if (!ErrorHandler.IsPrimaryKeyIsValid)
+            {
+                message = "Primary key is invalid";
+            }
+            else if (!ErrorHandler.IsMicConnected)
+            {
+                message = "Please set default microphone";
+            }
+            else
+            {
+                message = "connected";
+            }
+
+            MessageBox.Show(message);
+        }
     }
 }
---
+++ HumanVoiceToComputerVoice/SpeechRecognitionHandler.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ProjectOxford.SpeechRecognition;
+
+namespace HumanVoiceToComputerVoice
+{
+    #region SpeechRecognitionHandler
+
+    internal class SpeechRecognitionHandler
+    {
+        public string RecognizedToken { get; private set; }
+
+        private const string RecoLanguage = "en-us";
+        private readonly string _subscriptionKey = ConfigurationManager.AppSettings["primaryKey"];
+        private MicrophoneRecognitionClient _micClient;
+        private readonly MainWindow _mainWindow;
+
+        internal SpeechRecognitionHandler(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+
+            var errorHandler = mainWindow.ErrorHandler;
+            if (_subscriptionKey == null || !errorHandler.IsInternetValid || !errorHandler.Is64)
+            {
+                errorHandler.IsPrimaryKeyIsValid = false;
+                return;
+            }
+
+            try
+            {
+                if (_micClient != null)
+                    return;
+
+                if (mainWindow.TextBoxLongDict.IsChecked != null && !mainWindow.TextBoxLongDict.IsChecked.Value)
+                {
+                    _micClient = CreateMicrophoneRecoClient(SpeechRecognitionMode.ShortPhrase, RecoLanguage,
+                        _subscriptionKey);
+                }
+                else
+                {
+                    _micClient = CreateMicrophoneRecoClient(SpeechRecognitionMode.LongDictation, RecoLanguage,
+                        _subscriptionKey);
+                }
+            }
+            catch
+            {
+                errorHandler.IsMicConnected = false;
+            }
+        }
+
+        public void StartSpeechAndRecognition()
+        {
+            _micClient.StartMicAndRecognition();
+        }
+
+        private MicrophoneRecognitionClient CreateMicrophoneRecoClient(SpeechRecognitionMode recoMode, string language,
+            string subscriptionKey)
+        {
+            MicrophoneRecognitionClient micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                recoMode,
+                language,
+                subscriptionKey);
+
+            // Event handlers for speech recognition results
+            micClient.OnMicrophoneStatus += OnMicrophoneStatus;
+            micClient.OnPartialResponseReceived += OnPartialResponseReceivedHandler;
+
+            if (recoMode == SpeechRecognitionMode.ShortPhrase)
+            {
+                micClient.OnResponseReceived += OnMicShortPhraseResponseReceivedHandler;
+            }
+            else
+            {
+                micClient.OnResponseReceived += OnMicLongPhraseResponseReceivedHandler;
+            }
+
+            return micClient;
+        }
+
+        private void OnMicShortPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            _mainWindow.Dispatcher.Invoke((Action)(() =>
+            {
+                Console.WriteLine("--- OnMicShortPhraseResponseReceivedHandler ---");
+
+                _micClient.EndMicAndRecognition();
+
+                _micClient.Dispose();
+                _micClient = null;
+
+                WriteResponseResult(e);
+
+                UpdateUi();
+            }));
+        }
+
+        private void UpdateUi()
+        {
+            _mainWindow.TextRecordedToken.Text = RecognizedToken;
+
+            if (!string.IsNullOrEmpty(RecognizedToken))
+            {
+                _mainWindow.BtComputerSpeak.IsEnabled = true;
+            }
+
+            _mainWindow.BtMicrophone.IsEnabled = true;
+        }
+
+        private void OnMicLongPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            Console.WriteLine("--- OnMicLongPhraseResponseReceivedHandler ---");
+            if (e.PhraseResponse.RecognitionStatus == RecognitionStatus.EndOfDictation ||
+                e.PhraseResponse.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout)
+            {
+                _mainWindow.Dispatcher.Invoke((Action)(() =>
+                {
+                    UpdateUi();
+
+                    // we got the final result, so it we can end the mic reco.  No need to do this
+                    // for dataReco, since we already called endAudio() on it as soon as we were done
+                    // sending all the data.
+                }));
+            }
+            WriteResponseResult(e);
+        }
+
+        private void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            _mainWindow.Dispatcher.Invoke(() =>
+            {
+                Console.WriteLine("--- Microphone status change received by OnMicrophoneStatus() ---");
+                Console.WriteLine("********* Microphone status: {0} *********", e.Recording);
+                if (e.Recording)
+                {
+                    Console.WriteLine("Please start speaking.");
+                }
+                Console.WriteLine();
+            });
+        }
+
+        private void WriteResponseResult(SpeechResponseEventArgs e)
+        {
+            var recognizedPhrases = e.PhraseResponse.Results;
+            var length = recognizedPhrases.Length;
+            if (length == 0)
+            {
+                Console.WriteLine("No phrase resonse is available.");
+            }
+            else
+            {
+                Console.WriteLine("********* Final n-BEST Results *********");
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine("[{0}] Confidence={1}, Text=\"{2}\"",
+                        i, recognizedPhrases[i].Confidence,
+                        recognizedPhrases[i].DisplayText);
+                }
+
+                RecognizedToken = recognizedPhrases[0].DisplayText;
+
+                Console.WriteLine();
+            }
+        }
+
+        private void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            Console.WriteLine("--- Partial result received by OnPartialResponseReceivedHandler() ---");
+            Console.WriteLine("{0}", e.PartialResult);
+            Console.WriteLine();
+        }
+    }
+
+    #endregion
+}
